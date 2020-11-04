5a5e358a6bf87d2fb97676be77076204556362e2
Karel Asselman <asselman.karel@gmail.com>
2020-11-04 16:39:40 +0100

    CRUD functionality completed

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,18 +7,30 @@ class FlatsController < ApplicationController
   def show; end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    flat = Flat.new(flat_params)
+    if flat.save
+      redirect_to flat_path(flat)
+    else
+      render :new
+    end
   end
 
   def edit
+
   end
 
   def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
